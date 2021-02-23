72e168bc2e9d5649227fa2018c7b4539f8b6e33c
Louis Tuft <louistuft@hotmail.co.uk>
2021-02-23 16:00:03 +0000

    share code on GitHub

---
+++ frontend/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Product 1',
+            category:'Shirts',
+            image:'/images/louis.jpg',
+            price:25,
+            brand:'TeeMeUp',
+            rating:5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Product 2',
+            category:'Shirts',
+            image:'/images/louis.jpg',
+            price:25,
+            brand:'TeeMeUp',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Product 3',
+            category:'Shirts',
+            image:'/images/louis.jpg',
+            price:25,
+            brand:'TeeMeUp',
+            rating:4,
+            numReviews:10,
+            description:'High quality product'
+        },
+    ],
+};
+export default data;
